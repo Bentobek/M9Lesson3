d0500719f0495d26528c46738f7304e1351b2697
Bentobek <darhness12@gmail.com>
2025-06-29 22:24:33 +0600

    4 дз

--- app/src/main/java/com/example/m5lesson1/data/remote/ApiService.kt
+++ app/src/main/java/com/example/m5lesson1/data/remote/ApiService.kt
@@ -3,7 +3,7 @@ package com.example.m5lesson1.data.remote
 import com.example.m5lesson1.data.model.CharacterResponseDto
 import retrofit2.http.GET
 
-class ApiService {
+abstract class ApiService {
     @GET("character")
-    suspend fun getCharacters(): CharacterResponseDto
+    abstract suspend fun getCharacters(): CharacterResponseDto
 }
